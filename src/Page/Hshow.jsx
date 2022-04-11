9a1948a3f6adb815e5a047f7c5e256dfc32de417
Jyoti Prajapati <94469107+Rusta007@users.noreply.github.com>
2022-04-10 19:27:40 -1200

    Update Hshow.jsx

--- src/Page/Hshow.jsx
+++ src/Page/Hshow.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HData from './TData';
 
 
@@ -7,8 +8,8 @@ import HData from './TData';
 function Hshow(props){
     return<>
         <div className='block' style={{margin:"0px 0px 0px 20px"}}>
-        <a href='./home'>
-        <img src={props.img} alt="image" height={"200px"} width={"300px"} /></a>
+        <Link to='/'>
+        <img src={props.img} alt="image" height={"200px"} width={"300px"} /></Link>
         <h1 className='heading' style={{marginBottom:"0px"}} >{props.name1}</h1>
         <p>{props.detail}</p>
         <p className=''><span className='title'>Day of Visit </span>{props.contact}</p>
