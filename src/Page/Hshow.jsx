a6e290517893f5814c2bfcdce9c96ba137a950d2
Jyoti Prajapati <94469107+Rusta007@users.noreply.github.com>
2022-04-13 03:11:25 -1200

    Update Hshow.jsx

--- src/Page/Hshow.jsx
+++ src/Page/Hshow.jsx
@@ -8,9 +8,9 @@ import HData from './TData';
 function Hshow(props){
     return<>
         <div className='block' style={{margin:"0px 0px 0px 20px"}}>
-//         <Link to='/'>
+
         <img src={props.img} alt="image" height={"200px"} width={"300px"} />
-//             </Link>
+
         <h1 className='heading' style={{marginBottom:"0px"}} >{props.name1}</h1>
         <p>{props.detail}</p>
         <p className=''><span className='title'>Day of Visit </span>{props.contact}</p>
