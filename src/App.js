7cc87a1a083cea90f5e37f28a4d421bb5b8089fb
Jyoti Prajapati <94469107+Rusta007@users.noreply.github.com>
2022-05-17 09:12:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-// import { Home } from './Page/Home';
 import Main from './Page/Main';
 
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+    <div className="App"><Main /></div> );
 }
 
 export default App;
